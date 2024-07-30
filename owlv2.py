49fc3b9659f196251990332b743eb4fc80c57ffe
Bryce Goh <brycegohym@gmail.com>
2024-07-30 22:17:04 +0800

    Update owlv2.py

--- owlv2.py
+++ owlv2.py
@@ -25,61 +25,58 @@ class OwlSam:
     FUNCTION = "func"
 
     def func(self, images, texts, threshold):
-        try:
-            comfy_path = os.environ.get('COMFYUI_PATH')
-            if comfy_path is None:
-                comfy_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
-            
-            model_path = os.path.abspath(os.path.join(comfy_path, 'models'))
-            owl_path = os.path.join(model_path, 'owlsam', 'owlv2')
-            sam_path = os.path.join(model_path, 'owlsam', 'sam')
+        comfy_path = os.environ.get('COMFYUI_PATH')
+        if comfy_path is None:
+            comfy_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+        
+        model_path = os.path.abspath(os.path.join(comfy_path, 'models'))
+        owl_path = os.path.join(model_path, 'owlsam', 'owlv2')
+        sam_path = os.path.join(model_path, 'owlsam', 'sam')
 
-            detector = pipeline(task="zero-shot-object-detection", device="cuda", model=owl_path)
-            sam_model = SamModel.from_pretrained(sam_path).to("cuda")
-            sam_processor = SamProcessor.from_pretrained(sam_path)
+        detector = pipeline(task="zero-shot-object-detection", device="cuda", model=owl_path)
+        sam_model = SamModel.from_pretrained(sam_path).to("cuda")
+        sam_processor = SamProcessor.from_pretrained(sam_path)
 
-            # take image from first batch
-            image = images[0]
+        # take image from first batch
+        image = images[0]
 
-            texts = texts.split(",")
-            predictions = detector(
-                image,
-                candidate_labels=texts,
-                threshold=threshold
-            )
+        texts = texts.split(",")
+        predictions = detector(
+            image,
+            candidate_labels=texts,
+            threshold=threshold
+        )
 
-            result_labels = []
-            combined_mask = None
-            for pred in predictions:
-                box = pred["box"]
-                score = pred["score"]
-                label = pred["label"]
-                box = [round(pred["box"]["xmin"], 2), round(pred["box"]["ymin"], 2), 
-                    round(pred["box"]["xmax"], 2), round(pred["box"]["ymax"], 2)]
+        result_labels = []
+        combined_mask = None
+        for pred in predictions:
+            box = pred["box"]
+            score = pred["score"]
+            label = pred["label"]
+            box = [round(pred["box"]["xmin"], 2), round(pred["box"]["ymin"], 2), 
+                round(pred["box"]["xmax"], 2), round(pred["box"]["ymax"], 2)]
 
-                inputs = sam_processor(
-                        image,
-                        input_boxes=[[[box]]],
-                        return_tensors="pt"
-                    ).to("cuda")
+            inputs = sam_processor(
+                    image,
+                    input_boxes=[[[box]]],
+                    return_tensors="pt"
+                ).to("cuda")
 
-                with torch.no_grad():
-                    outputs = sam_model(**inputs)
+            with torch.no_grad():
+                outputs = sam_model(**inputs)
 
-                mask = sam_processor.image_processor.post_process_masks(
-                    outputs.pred_masks.cpu(),
-                    inputs["original_sizes"].cpu(),
-                    inputs["reshaped_input_sizes"].cpu()
-                )[0][0][0].numpy()
-                mask = mask[np.newaxis, ...]
-                result_labels.append((mask, label))
+            mask = sam_processor.image_processor.post_process_masks(
+                outputs.pred_masks.cpu(),
+                inputs["original_sizes"].cpu(),
+                inputs["reshaped_input_sizes"].cpu()
+            )[0][0][0].numpy()
+            mask = mask[np.newaxis, ...]
+            result_labels.append((mask, label))
+            print(mask)
+            if combined_mask is None:
+                combined_mask = mask
+            else:
+                combined_mask = np.logical_or(combined_mask, mask)
 
-                if combined_mask is None:
-                    combined_mask = mask
-                else:
-                    combined_mask = np.logical_or(combined_mask, mask)
-
-            combined_mask = torch.from_numpy(combined_mask).unsqueeze(0)
-            return (combined_mask,)
-        except Exception as e:
-            raise Exception(f"Error processing image: {str(e)}")
+        combined_mask = torch.from_numpy(combined_mask).unsqueeze(0)
+        return (combined_mask,)
