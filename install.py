060c9042d90a37a99ba3e4551263e2b419a197bb
Bryce Goh <brycegohym@gmail.com>
2024-07-31 17:21:17 +0800

    Update install.py

--- install.py
+++ install.py
@@ -12,10 +12,10 @@ def main():
   if not os.path.exists(model_path):
     os.makedirs(model_path)
   print(f"Set HF_HOME to {model_path}")
-  SamModel.from_pretrained("facebook/sam-vit-base")
-  SamProcessor.from_pretrained("facebook/sam-vit-base")
-  AutoProcessor.from_pretrained("google/owlv2-base-patch16-ensemble")
-  Owlv2ForObjectDetection.from_pretrained("google/owlv2-base-patch16-ensemble")
+  SamModel.from_pretrained("facebook/sam-vit-base", cache_dir=model_path)
+  SamProcessor.from_pretrained("facebook/sam-vit-base", cache_dir=model_path)
+  AutoProcessor.from_pretrained("google/owlv2-base-patch16-ensemble", cache_dir=model_path)
+  Owlv2ForObjectDetection.from_pretrained("google/owlv2-base-patch16-ensemble", cache_dir=model_path)
 
 if __name__ == "__main__":
     main()
